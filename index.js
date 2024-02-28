74c1a443ac8300cca2a86b01987d7d911da4f3eb
William <mauritius.william.wijaya1@gmail.com>
2024-02-28 22:39:14 +0700

    belajar upload ke github

---
+++ index.js
@@ -0,0 +1,9 @@
+//? PENGEN KE WARNET
+
+var uang = 200_001
+var hargaWarnet = 200_000
+var dikasihPinjam = true
+
+if (uang >= hargaWarnet) console.log("bisa maen warnet");
+else if (dikasihPinjam) console.log("bisa maen warnet");
+else console.log("pulang");
